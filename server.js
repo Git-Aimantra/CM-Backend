9b58c8e95f24cfb76a88f5376cb32ae37aaf4e29
shh-ubham10 <167060474+shh-ubham10@users.noreply.github.com>
2026-09-16 16:32:20 +0530

    FIle size increase

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use("/api/investor", investorRoutes);
 app.use("/api/career", careerRoutes);
 app.use("/api/projects", projectRoutes);
